311fd8354e53e640c0d24cb3b46a08f2e77ee9d3
sun <asmh1989@gmail.com>
2018-04-27 10:08:03 +0800

    ios 图标

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,12 +1,15 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import <Bugly/Bugly.h>
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+    [GeneratedPluginRegistrant registerWithRegistry:self];
+    [Bugly startWithAppId:@"c68b6c9b3d"];
+    
+    // Override point for customization after application launch.
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
 @end
